19f618f2396d1f387882c49d93033e6fda75b552
Celine Duflo <cduflo@hotmail.com>
2019-06-24 19:07:26 +0200

    Initial Commit

---
+++ playcard2Tests/playcard2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  playcard2Tests.swift
+//  playcard2Tests
+//
+//  Created by Céline Duflo on 24/06/2019.
+//  Copyright © 2019 Céline Duflo. All rights reserved.
+//
+
+import XCTest
+@testable import playcard2
+
+class playcard2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
