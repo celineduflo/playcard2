b5f4c2a616d735717879299881c699f729ba0f89
Celine Duflo <cduflo@hotmail.com>
2019-06-24 19:45:20 +0200

    1st commit

--- playcard2/ViewController.swift
+++ playcard2/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var status: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func changeToRed(_ sender: Any) {
+        changeRed()
+    }
+    
+    @IBAction func changeToBlack(_ sender: Any) {
+        changeBlack()
+    }
+    
+    func changeRed() {
+        if status.text == "♦️" || status.text == "♠️" || status.text == "♣️"  {
+            status.text = "♥️"
+        }
+        
+        if status.text == "♥️" || status.text == "♠️" || status.text == "♣️"  {
+            status.text = "♦️"
+        }
+    }
+    
+    func changeBlack() {
+        if status.text == "♠️" || status.text == "♥️" || status.text == "♦️"  {
+            status.text = "♣️"
+        }
+        
+        if status.text == "♣️" || status.text == "♥️" || status.text == "♦️"  {
+            status.text = "♠️"
+        }
+    }
 
-
+//♥️♦️♠️♣️
 }
 
